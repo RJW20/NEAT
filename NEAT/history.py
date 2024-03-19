c5148a51ba946c00c9a134158df45650d3f5d775
RJW20 <robertjwatts84@outlook.com>
2024-03-19 11:55:02 +0000

    Make history checking more explicit

--- NEAT/history.py
+++ NEAT/history.py
@@ -10,12 +10,13 @@ class History:
         self.innovations: list[Innovation] = []
 
     @property
-    def next_innovation(self) -> int:
+    def next_innovation_number(self) -> int:
         """The number to assign to the next new Innovation."""
         return len(self.innovations)
 
     def get_innovation_number(self, genome: Genome, from_node: Node, to_node: Node) -> int:
-        """Return the innovation number for a mutation.
+        """Return the innovation number for a Genome mutation that is making a new Connection 
+        (new Node or new Connection mutation).
         
         If the mutation is the first of its kind it will be assigned a new number, else 
         it will be matched up with a previous Innovation.
@@ -27,6 +28,6 @@ class History:
                 return innovation.number
             
         # Create a new Innovation
-        new_innovation = Innovation(self.next_innovation, genome, from_node, to_node)
+        new_innovation = Innovation(self.next_innovation_number, genome, from_node, to_node)
         self.innovations.append(new_innovation)
         return new_innovation.number
