6df8aab75e88963d3578947bdfb1ba65ea3357a9
RJW20 <robertjwatts84@outlook.com>
2024-03-19 11:31:49 +0000

    Add innovation match checker

--- NEAT/innovation.py
+++ NEAT/innovation.py
@@ -1,3 +1,4 @@
+from NEAT.genome import Genome
 from NEAT.node import Node
 
 
@@ -9,6 +10,24 @@ class Innovation:
     uniquely identifies a new Connection between two Nodes.
     """
 
-    def __init__(self) -> None:
-        self.number: int
-        self.state_occurred: list[int]
+    def __init__(self, number: int, genome: Genome, from_node: Node, to_node: Node) -> None:
+        self.number: int = number
+        self.present_connections: list[int] = [connection.innovation_number for connection in genome.connections]
+        self.from_node_number: int = from_node.number
+        self.to_node_number: int = to_node.number
+
+    def match(self, genome: Genome, from_node: Node, to_node: Node) -> bool:
+        """Return True if a Genome that is about to make a new Connection matches 
+        this Innovation."""
+
+        if not (self.from_node_number == from_node.number and self.to_node_number == to_node.number):
+            return False
+
+        if len(self.present_connections) != len(genome.connections):
+            return False
+        
+        for connection in genome.connections:
+            if connection.innovation_number not in self.present_connections:
+                return False
+            
+        return True
