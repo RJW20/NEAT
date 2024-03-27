201974b3f01538fd37fb54e263b1811f76c557ea
RJW20 <robertjwatts84@outlook.com>
2024-03-27 11:45:06 +0000

    Add basic attributes to Population and Species classes

--- NEAT/population/population.py
+++ NEAT/population/population.py
@@ -1,8 +1,22 @@
-from NEAT.species import Species
+from NEAT.population.species import Species
+from NEAT.history.history import History
 
 
 class Population:
-    """Contains all the different Species."""
+    """Contains all the different Species.
+    
+    Must be initiated with a settings dictionary as found in the documentation.
+    """
 
-    def __init__(self) -> None:
+    def __init__(self, settings: dict) -> None:
         self.species: list[Species]
+        self.history: list[History]
+
+        # Unload the settings
+        try:
+            self._excess_coefficient = settings['excess_coefficient']
+            self._disjoint_coefficient = settings['disjoint_coefficient']
+            self._weight_difference_coefficient = settings['weight_difference_coefficient']
+            self._compatibility_threshold = settings['compatibility_threshold']
+        except KeyError as e:
+            print(f'Setting {e.args[0]} not found in settings.')
--- NEAT/population/species.py
+++ NEAT/population/species.py
@@ -5,6 +5,20 @@ from NEAT.genome import Genome
 class Species:
     """Contains a large group of Players that have similar Genomes."""
 
-    def __init__(self, player: BasePlayer) -> None:
+    def __init__(self, player: BasePlayer, **settings: dict) -> None:
         self.rep: Genome
         self.players: list[BasePlayer]
+
+        # Unload the settings
+        self._c1 = settings['c1']
+        self._c2 = settings['c2']
+        self._c3 = settings['c3']
+        self._delta = settings['delta']
+
+    def same_species(self, genome: Genome) -> bool:
+        """Return True if the given Genome is considered to be a part of this Species.
+        
+        This is determined by calculating the compatibility distance from this Species' rep 
+        and comparing it to the chosen compatibility threshold.
+        """
+
