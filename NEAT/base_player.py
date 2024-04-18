c1a0cca8a5894a90670c6742342f0d9e4b9c2e24
RJW20 <robertjwatts84@outlook.com>
2024-04-18 13:47:21 +0100

    Add abstract methods to BasePlayer class

--- NEAT/base_player.py
+++ NEAT/base_player.py
@@ -1,4 +1,5 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+from typing import Any
 from __future__ import annotations
 
 from NEAT.genome import Genome
@@ -14,3 +15,18 @@ class BasePlayer(ABC):
         self.fitness: float
         self.adjusted_fitness: float
         self.genome: Genome
+
+    @abstractmethod
+    def look(self) -> None:
+        """Update the attributes used as input to the Genome."""
+        pass
+
+    @abstractmethod
+    def think(self) -> Any:
+        """Feed the input into the Genome and return the output as a valid move."""
+        pass
+
+    @abstractmethod
+    def move(self, move: Any) -> None:
+        """Advance to the state achieved by carrying out move."""
+        pass
